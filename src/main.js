c873b4cda0027bb954fd640ee9846b3f586db4fc
吕惠婷 <ruoyuting@qq.com>
2020-04-28 19:59:00 +0800

    完成了登录功能

--- src/main.js
+++ src/main.js
@@ -2,6 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+// 导入字体图标
+import './assets/fonts/iconfont.css'
+// 导入全局样式表
+import './assets/css/global.css'
+
+// 在请求接口之前，需要全局配置 axios 包
+import axios from 'axios'
+// 配置请求根路径/接口基准路径（在API接口文档中）
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+// 把 axios 包挂载到 Vue 的原型对象上
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,10 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+import { Button, Form, FormItem, Input, Message } from 'element-ui'
 
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+// Message 和其他组件不同，它需要全局挂载为 Vue 原型中的一个属性，这样每个 Vue 组件可通过 this 访问到 $message
+// 这里的 $message 是自定义属性，可自己改名
+Vue.prototype.$message = Message
